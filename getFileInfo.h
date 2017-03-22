4181cee065d0edcde66883da921ef675b6abcc87
Luke William Duane <lduane@nd.edu>
2017-03-22 00:17:35 -0400

    fixed file opening stuff

--- getFileInfo.h
+++ getFileInfo.h
@@ -19,12 +19,20 @@ class getFileInfo {
 
 getFileInfo::getFileInfo (string s) {
 	fileName = s;
-	
+        /*	
 	FILE *fp;
 	char str[100];
 	fp = fopen(fileName.c_str(), "r");
 	while(fgets(str,100,fp)!= NULL){
 		allFiles.push_back(str);
 	}
-	fclose(fp);
+        */
+        string tmp;
+        ifstream myfile(fileName.c_str());
+        if(myfile.good()){
+            while(getline(myfile, tmp)){
+                allFiles.push_back(tmp);
+            }
+        }
+        myfile.close();
 }
