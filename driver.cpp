d89201dbbe4ba78110ae6218916cd592c8ca3719
jbruscia <jbruscia@nd.edu>
2017-03-22 14:13:49 -0700

    timer stuff

--- driver.cpp
+++ driver.cpp
@@ -3,9 +3,62 @@
 #include "configFile.h"
 #include "getFileInfo.h"
 #include "Curl.h"
+#include <signal.h>
+#include <errno.h>
+#include <unistd.h>
+
+
 
 using namespace std;
 
+char bKeepLooping = 1;
+char bKeepLooping2 = 1;
+int countPhrase(string page, string phrase);
+void runThreading(configFile conf);
+void handler(int a) {
+	bKeepLooping = 0;
+	bKeepLooping2 = 0;
+}
+void handler2(int a) {
+	bKeepLooping2 = 0;
+}
+
+int main(){
+	configFile conf;
+	cout << "search file: " << conf.get_SEARCH_FILE() << endl;
+	cout << "site file: " << conf.get_SITE_FILE() << endl;
+	
+	alarm(conf.get_PERIOD_FETCH());
+	int period = conf.get_PERIOD_FETCH();
+	signal(SIGINT, handler);
+	signal(SIGALRM, handler2);
+	while (bKeepLooping) {
+		while (bKeepLooping2) {
+			
+		}
+		alarm(period);
+		bKeepLooping2 = 1;
+		runThreading(conf);
+	}
+
+	return 0;
+}
+
+void runThreading(configFile conf) {
+	Curl c;
+	string s; string t;
+	getFileInfo search(conf.get_SEARCH_FILE());
+	getFileInfo site(conf.get_SITE_FILE());
+	
+	
+	
+	
+	cout << site.getVector()[0] << endl;
+	s = c.fetch(site.getVector()[0]);
+	t = search.getVector()[0];
+	cout << countPhrase(s,t) << endl;
+	
+}
 
 int countPhrase(string page, string phrase) {
 	int found;
@@ -18,24 +71,4 @@ int countPhrase(string page, string phrase) {
 		//cout << found << endl;
 	}
 	return count;
-}
-
-int main(){
-   Curl c;
-   string s = "dad";
-   string t = "d";
-   //cout << c.fetch("http://nd.edu") << endl;
-   configFile conf;
-   cout << "serach file: " << conf.get_SEARCH_FILE() << endl;
-   cout << "site: " << conf.get_SITE_FILE() << endl;
-   string searchFile = conf.get_SEARCH_FILE();
-   cout << conf.get_SITE_FILE();
-   getFileInfo search(searchFile);
-   getFileInfo site(conf.get_SITE_FILE());
-   cout << site.getVector()[0] << endl;
-   s = c.fetch(site.getVector()[0]);
-   t = search.getVector()[0];
-   cout << countPhrase(s,t) << endl;
-
-   
 }
