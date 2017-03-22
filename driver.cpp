51ae93ac69415eb6e5c38edf2b13558055b41ae5
John Marco Bruscia <jbruscia@student02.cse.nd.edu>
2017-03-22 00:38:55 -0400

    dlks

--- driver.cpp
+++ driver.cpp
@@ -22,17 +22,19 @@ int countPhrase(string page, string phrase) {
 
 int main(){
    Curl c;
-   string s;
-   string t;
+   string s = "dad";
+   string t = "d";
    //cout << c.fetch("http://nd.edu") << endl;
    configFile conf;
    cout << "serach file: " << conf.get_SEARCH_FILE() << endl;
    cout << "site: " << conf.get_SITE_FILE() << endl;
    string searchFile = conf.get_SEARCH_FILE();
    getFileInfo search(searchFile);
-   //getFileInfo site(conf.get_SITE_FILE());
-   /*s = c.fetch(site.getVector()[0]);
-   t = search.getVector()[0];
+   getFileInfo site(conf.get_SITE_FILE());
+   cout << site.getVector()[0] << endl;
+   s = c.fetch(site.getVector()[0]);
+   //t = search.getVector()[0];
    cout << countPhrase(s,t) << endl;
-   */
+
+   
 }
